3bc04cb4fff812d1d768f4f2fa3669900b762ceb
David Sean <david.sean@gmail.com>
2024-09-19 16:37:12 -0400

    add script to batch add photos from biigle

---
+++ scripts.py
@@ -0,0 +1,54 @@
+import glob
+import requests
+from requests.auth import HTTPBasicAuth
+
+from images.models import Image
+from shared_models.models import Set
+from shared_models.utils import get_active_cruise
+from django.core.files.images import ImageFile
+
+def get_biigle_photos():
+    email = "david.sean-fortin@dfo-mpo.gc.ca"
+    token = "P9JNvgVsX5lEIztm5PtNpu6QsLWvouC0"
+    headers = {'Accept': 'application/json'}
+    auth = HTTPBasicAuth(email, token)
+    base_url ="https://biigle.de/api"
+
+    volume_id = 19384
+    endpoint=f"/v1/volumes/{volume_id}/filenames"
+    # get filenames
+    response = requests.get(f"{base_url}{endpoint}", headers=headers, auth=auth)
+
+
+    if not response.status_code==200: 
+        print("bad response")
+        exit()
+
+    for img_id, filename in response.json().items():
+        print(img_id)    
+        print(filename)
+
+        endpoint = f"/v1/images/{img_id}/file"
+        response = requests.get(f"{base_url}{endpoint}", headers=headers, auth=auth)
+        with open(filename, 'wb') as fp:
+            fp.write(response.content)
+
+
+def add_photos():
+
+    cruise = get_active_cruise()
+    
+    for fname in glob.glob("andesOBIS/*.jpg"):
+        set_num = fname.split("_")[1]
+        set = Set.objects.get(set_number=set_num, cruise=cruise)
+
+        with open(fname, 'rb') as fp:
+            image_file = ImageFile(fp)
+            if image_file:
+                image = Image(set=set, andes_uuid=set.uuid, type='set',image=image_file)
+                image.save(image_file.name, image_file)
+
+
+if __name__ == "__main__":
+    # get_biigle_photos()
+    add_photos()
